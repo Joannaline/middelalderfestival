f41cf973c534cf9b709264469c8f0db44e6b8989
anettenicolaisen5 <anettenicolaisen5@hotmail.com>
2018-05-22 10:12:01 +0200

    javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,42 +2,50 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
+    var slides = document.querySelectorAll(".mySlides");
+    if(slides.length > 0)
+    {
+      var dots = document.querySelectorAll("dot");
+      for (var i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      for (var i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 10000);
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 10000);
 }
+
 
 //frivillig modal
 
 /* Definering af modal*/
-var modal = document.getElementById('myModal');
+var modal = document.querySelector('#myModal');
 
 /* definering af knappen der åbner modalen*/
-var btn = document.getElementById("myBtnone");
+var btn = document.querySelector("#myBtnone");
 
 /* span elementer(krydset oppe i hjørnet)(html) der lukker modalen*/
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelector(".close");
+
+if(span !== null)
+{
+  /* når man trykker på X (span element), så lukker modalen*/
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+}
 
 /* funktionen der gør at når man trykker på knappen så åbner modalen*/
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-/* når man trykker på X (span element), så lukker modalen*/
-/span.onclick = function() {
-    modal.style.display = "none";
-}
 
 /* når man trykker uden for modalen så lukker modalen ned*/
 window.onclick = function(event) {
